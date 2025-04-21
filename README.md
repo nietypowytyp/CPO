e8e70fc9a58d051fd041911e4d7e556ecb8fa70e
unknown <01150909@pw.edu.pl>
2025-04-21 16:03:16 +0200

    feat: python script for dowloading dataset

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Poject log:
 250406 - wgranie zdjęć do rozpoznania
+250421 - setup środowiska (conda, torch nightly for CUDA 12.8)
--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import torch
+import os
+from zipfile import ZipFile
 
-print(torch.__version__)
-print(torch.cuda.is_available())
+# Set Kaggle API credentials
+kaggle_dir = os.path.expanduser("~/.kaggle")
+os.makedirs(kaggle_dir, exist_ok=True)
+
+# Make sure your kaggle.json is already in the current dir
+os.system("mv kaggle.json ~/.kaggle/")
+os.chmod(os.path.join(kaggle_dir, 'kaggle.json'), 0o600)
+
+# Download the dataset
+os.system("kaggle datasets download -d ritikagiridhar/2000-hand-gestures")
+
+# Create project data directory
+project_data_path = "project_dir/hand_gestures"
+os.makedirs(project_data_path, exist_ok=True)
+
+# Unzip the downloaded file
+with ZipFile("2000-hand-gestures.zip", 'r') as zip_ref:
+    zip_ref.extractall(project_data_path)
+
+print(f"✅ Dataset extracted to {project_data_path}")
